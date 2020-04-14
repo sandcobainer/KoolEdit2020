31f8eeac5e5899b78a93f58ea29314f1782a8f7d
Sophia mehdizadeh <smehdizadeh7@gatech.edu>
2020-04-13 21:39:49 -0400

    skeleton for selection class/component

---
+++ WaveEditor_Group1/Source/Selection.h
@@ -0,0 +1,42 @@
+/*
+  ==============================================================================
+
+    Selection.h
+    Created: 13 Apr 2020 9:29:12pm
+    Author:  sophi
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "AudioProcessingComponent.h"
+
+class Selection : public Component
+{
+public:
+    Selection()
+    {
+
+    }
+
+    ~Selection()
+    {
+
+    }
+
+    void paint(Graphics& g) override
+    {
+
+    }
+
+    void resized() override
+    {
+
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Selection)
+
+};
