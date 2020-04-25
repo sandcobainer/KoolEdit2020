bec157bd77562c14402c206db8a1bdfbfbdaea1e
Yilin Zhang <yzhang0129@outlook.com>
2020-04-25 01:20:58 -0400

    Add SpectrogramProcessor.h

---
+++ WaveEditor_Group1/Source/SpectrogramProcessor.h
@@ -0,0 +1,103 @@
+/*
+  ==============================================================================
+
+    SpectrogramProcessor.h
+    Created: 7 Feb 2020 2:29:30pm
+    Author:  sophi
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "AudioProcessingComponent.h"
+
+class SpectrogramProcessor : public ChangeListener,
+{
+public:
+    SpectrogramProcessor(AudioProcessingComponent& c):
+    apc(c),
+    forwardFFT (fftOrder),
+    matrixRawPointer(nullptr),
+    numRows(0),
+    numColumns(0),
+    isMatrixInitialized(false)
+    {
+        apc.audioBufferChanged.addChangeListener(this);
+    }
+    ~SpectrogramProcessor ()
+    {
+        if (isMatrixInitialized)
+            releaseMemForMatrix();
+    }
+
+    enum
+    {
+        fftOrder = 10,
+        fftSize  = 1 << fftOrder
+    };
+
+    const float* getReadPointer()
+    {
+        return const_cast<float*> (matrixRawPointer);
+    }
+
+    int getNumRows()
+    {
+        return numRows;
+    }
+
+    int getNumColumns()
+    {
+        return numColumns;
+    }
+
+    void changeListenerCallback (ChangeBroadcaster* source) override
+    {
+        int numSamples;
+        int cols = fftSize * 2;
+        // TODO: it only takes channel 0
+        auto readPointer = apc.getAudioReadPointer(0, numSamples);
+        releaseMemForMatrix();
+        // the size of each row must be fftSize*2
+        allocMemForMatrix(static_cast<int>(numSamples/fftSize), cols);
+        zeromem(matrixRawPointer, sizeof(float)*getNumRows()*getNumColumns());
+        for (int row=0; row<getNumRows(); row++)
+        {
+            // [----------]  <- [*****]
+            // [*****-----]
+            memcpy(matrixRawPointer+row*cols, readPointer+row*fftSize, sizeof(float)*fftSize);
+            forwardFFT.performFrequencyOnlyForwardTransform(matrixRawPointer+row*cols);
+        }
+        spectrogramReady.sendChangeMessage();
+    }
+
+    ChangeBroadcaster spectrogramReady;
+
+private:
+    void allocMemForMatrix(int numRows, int numColumns)
+    {
+        matrixRawPointer = new float[numRows*numColumns];
+        this->numRows = numRows;
+        this->numColumns = numColumns;
+        isMatrixInitialized = true;
+    }
+
+    void releaseMemForMatrix()
+    {
+        delete  matrixRawPointer;
+        matrixRawPointer = nullptr;
+        numRows = 0;
+        numColumns = 0;
+        isMatrixInitialized = false;
+    }
+
+    AudioProcessingComponent& apc;
+    dsp::FFT forwardFFT;
+
+    float* matrixRawPointer; // spectrogram matrix, raw*col, each raw represents a block
+    int numRows;
+    int numColumns;
+    bool isMatrixInitialized;
+};
